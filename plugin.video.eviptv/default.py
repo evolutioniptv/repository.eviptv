17ee664708fea3eb73a3a0f9020d97a1c83544c4
Jason <mosb3rgler@gmail.com>
2019-09-28 00:26:34 -0400

    domain + port update
    With the XC system being reverted back to v1.0.6 the domain and port needed to be updated to accomodate the specific port requirement.

--- plugin.video.eviptv/default.py
+++ plugin.video.eviptv/default.py
@@ -27,8 +27,8 @@ ICON = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'ico
 FANART = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'fanart.jpg')) 
 APKS = base64.b64decode("aHR0cDovL2ZhYmlwdHYuY29tL2Fwa3MvbmV3YXBrcy50eHQ=")
 HOME =  xbmc.translatePath('special://home/')
-lehekylg= base64.b64decode("aHR0cDovL3dhdGNoLmV2b2x1dGlvbmlwdHYubmluamE=")
-pordinumber="80"
+lehekylg= base64.b64decode("ZXZvbHV0aW9uaXB0di5uaW5qYQ==")
+pordinumber="8888"
 message = "VU5BVVRIT1JJWkVEIEVESVQgT0YgQURET04h"
 kasutajanimi=plugintools.get_setting("Username")
 salasona=plugintools.get_setting("Password")
